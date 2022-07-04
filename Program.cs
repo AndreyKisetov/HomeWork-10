3e6da608da386bfa4627b1e436c3ffe00e9e6bdf
Andrey Kisetov <rbcfff@yandex.ru>
2022-07-04 16:56:12 +0300

    added Program.cs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Hello
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+            {
+            Console.Clear();
+            Console.WriteLine("Введите целое положительное трёхзначное число: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            string x = Convert.ToString(number);
+            Console.WriteLine($"-> {x[1]}");
+            }
+        }
+    }
+}
